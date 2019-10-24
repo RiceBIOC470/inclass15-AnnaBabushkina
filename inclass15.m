2e8b727550482eeed2bd005179cc0df5eb0c1c70
warmflasha <warmflasha@gmail.com>
2019-10-24 11:25:02 -0500

    Update

--- inclass15.m
+++ inclass15.m
@@ -5,6 +5,6 @@
 % (1) Make a binary mask by thresholding as best you can
 % (2) Try to separate touching objects using watershed. Use imerode 
 % to break up centers, and apply the resulting mask in two different
-% ways to define the basins for watershed. (A) To the intensity image 
-% (B) To the distance transform of the mask.
+% ways to define the basins for watershed. (A) Using the intensity image
+% (B) Using the distance transform of the mask.
 % Which works better in this case?
