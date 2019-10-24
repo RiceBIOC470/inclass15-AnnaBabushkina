462b1cf71904d4043b1fb479c5d6638488b5be5b
warmflasha <warmflasha@gmail.com>
2019-10-24 10:42:36 -0500

    Added inclass15

---
+++ inclass15.m
@@ -0,0 +1,8 @@
+%Inclass 15
+
+%Work with the image stemcells_dapi.tif in this folder
+
+% (1) Make a binary mask by thresholding as best you can
+% (2) Try to separate touching objects using watershed. Use two different
+% ways to define the basins. (A) With erosion of the mask (B) with a
+% distance transform. Which works better in this case?
